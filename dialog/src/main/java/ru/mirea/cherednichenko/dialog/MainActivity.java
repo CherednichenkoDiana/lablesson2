afb35d530d63b733b7ed6e07f8e398187cddbade
CherednichenkoDiana <allison.chees@gmail.com>
2019-11-22 23:52:26 +0300

    ver2

--- dialog/src/main/java/ru/mirea/cherednichenko/dialog/MainActivity.java
+++ dialog/src/main/java/ru/mirea/cherednichenko/dialog/MainActivity.java
@@ -3,6 +3,7 @@ package ru.mirea.cherednichenko.dialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -15,4 +16,16 @@ public class MainActivity extends AppCompatActivity {
         LOL dialogFragment = new LOL ();
         dialogFragment.show(getSupportFragmentManager(), "dialog");
     }
+    public void onOkClicked() {
+        Toast.makeText(getApplicationContext(), "Вы выбрали кнопку \"Иду дальше\"!",
+                Toast.LENGTH_LONG).show(); }
+
+    public void onCancelClicked() {
+        Toast.makeText(getApplicationContext(), "Вы выбрали кнопку \"Нет\"!",
+                Toast.LENGTH_LONG).show(); }
+
+    public void onNeutralClicked() {
+        Toast.makeText(getApplicationContext(), "Вы выбрали кнопку \"На паузе\"!",
+                Toast.LENGTH_LONG).show(); }
 }
+
