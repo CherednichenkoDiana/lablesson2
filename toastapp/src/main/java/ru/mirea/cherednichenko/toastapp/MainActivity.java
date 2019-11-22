29b740bd566a46d8b8db2cc7c11a8023067bbf05
CherednichenkoDiana <allison.chees@gmail.com>
2019-11-22 13:12:14 +0300

    ver2

--- toastapp/src/main/java/ru/mirea/cherednichenko/toastapp/MainActivity.java
+++ toastapp/src/main/java/ru/mirea/cherednichenko/toastapp/MainActivity.java
@@ -1,8 +1,12 @@
 package ru.mirea.cherednichenko.toastapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Toast;
+import android.widget.LinearLayout;
+import android.widget.ImageView;
+import android.view.Gravity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "Здравствуй MIREA!",
+                Toast.LENGTH_SHORT);
+
+        toast.setGravity(Gravity.BOTTOM, 0, 900);
+        LinearLayout toastContainer = (LinearLayout) toast.getView();
+        ImageView catImageView = new ImageView(getApplicationContext());
+        catImageView.setImageResource(R.drawable.ic_launcher_background);
+        toastContainer.addView(catImageView, 0);
+        toast.show();
     }
 }
